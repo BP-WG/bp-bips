42c3ba7c9e4c20bcfdb4c49a68764a1403e1486d
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-02-18 04:24:37 +0100

    data: add key type enumerations

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,40 @@
 //! Zero-dependency no-std 100% standard-compliant PSBT v0 and v2 implementation.
 
+pub trait PsbtKeyType {}
+
+pub enum InKey {}
+impl PsbtKeyType for InKey {}
+
+pub enum OutKey {}
+impl PsbtKeyType for OutKey {}
+
+pub enum GlobalKey {}
+impl PsbtKeyType for GlobalKey {}
+
+pub struct UnknownKey(u64);
+impl PsbtKeyType for UnknownKey {}
+
 pub struct Psbt {
-    global: GlobalMap,
-    inputs: Vec<InputMap>,
-    outputs: Vec<OutputMap>,
+    global: KeyMap<GlobalKey>,
+    inputs: Vec<KeyMap<InKey>>,
+    outputs: Vec<KeyMap<OutKey>>,
 }
 
-pub struct GlobalMap(Vec<KeyPair>);
+pub struct KeyMap<T: PsbtKeyType>(Vec<KeyPair<T>>);
 
-pub struct InputMap(Vec<KeyPair>);
-
-pub struct OutputMap(Vec<KeyPair>);
-
-pub struct KeyPair {
-    pub key: Key,
+pub struct KeyPair<T: PsbtKeyType> {
+    pub key: Key<T>,
     pub value: Vec<u8>
 }
 
-pub enum Key {
-    Standard(StandardKey),
+pub enum Key<T: PsbtKeyType> {
+    Known(CommonKey<T>),
+    Unknown(CommonKey<UnknownKey>),
     Proprietary(ProprietaryKey),
 }
 
-pub struct StandardKey {
-    pub key_type: u64,
+pub struct CommonKey<T: PsbtKeyType> {
+    pub key_type: T,
     pub key_data: Vec<u8>,
 }
 
@@ -31,4 +42,7 @@ pub struct ProprietaryKey {
     pub identifier: String,
     pub subtype: u64,
     pub key_data: Vec<u8>,
+}
+
+pub enum KeyType {
 }
